58b295f46b70b0920aeb2bc0ee7eafd85cc321bf
riv4l23@protonmail.com <51548322+Rival420@users.noreply.github.com>
2023-10-26 23:24:28 +0200

    Update TrackScan.py

--- Initial_enum/TrackScan.py
+++ Initial_enum/TrackScan.py
@@ -2,44 +2,76 @@ import subprocess
 import re
 import argparse
 
-def run_command(command):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    stdout, _ = process.communicate()
-    return stdout.decode('utf-8')
+def run_command(command, title, verbose):
+    ports = {}
+    print(f"[*] Running {title}...")
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+    print(f"[*] {title} results:")
+    while True:
+        output = process.stdout.readline()
+        if output:
+            line = output.strip()
+            if verbose:
+                print(f"    {line}")
+            match = re.search(r"(\d+)/(tcp|udp)\s+(\w+)", line)
+            if match:
+                port, protocol, state = match.groups()
+                ports[f"{port}/{protocol}"] = state
+        if process.poll() is not None:
+            break
 
-def get_open_ports(nmap_output):
-    open_ports = []
-    lines = nmap_output.strip().split("\n")
-    for line in lines:
-        match = re.search(r"(\d+)/tcp\s+open", line)
-        if match:
-            open_ports.append(match.group(1))
-    return ",".join(open_ports)
+    if ports:
+        print("PORT       STATE")
+        for port, state in ports.items():
+            print(f"{port.ljust(9)} {state}")
 
-def main(target):
-    # Initial full port scan
-    print("[*] Running initial port scan...")
-    initial_scan_command = f"nmap -p- --min-rate=1000 --max-retries=1 {target}"
-    initial_scan_output = run_command(initial_scan_command)
+    return ",".join([port.split("/")[0] for port in ports.keys() if ports[port] == 'open'])
 
-    # Parsing the initial output to get the open ports
-    print("[*] Parsing initial scan output...")
-    open_ports = get_open_ports(initial_scan_output)
-    if not open_ports:
-        print("[!] No open ports found. Exiting.")
-        return
+def quick_scan(target, pn_flag, verbose):
+    return run_command(f"nmap -F {'-Pn' if pn_flag else ''} {target}", "quick scan", verbose)
 
-    # Running a detailed scan on the open ports
+def full_tcp_scan(target, pn_flag, verbose):
+    return run_command(f"nmap -p- --min-rate=1000 --max-retries=1 {'-Pn' if pn_flag else ''} {target}", "full TCP scan", verbose)
+
+def full_udp_scan(target, pn_flag, verbose):
+    return run_command(f"nmap -sU --top-ports 20 {'-Pn' if pn_flag else ''} {target}", "full UDP scan", verbose)
+
+def detailed_scan(target, open_ports, pn_flag, verbose):
     print(f"[*] Running detailed scan on open ports: {open_ports}...")
-    detailed_scan_command = f"nmap -p {open_ports} -sC -sV {target}"
-    detailed_scan_output = run_command(detailed_scan_command)
-
+    detailed_scan_command = f"nmap -p {open_ports} -sC -sV {'-Pn' if pn_flag else ''} {target}"
+    process = subprocess.Popen(detailed_scan_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, text=True)
+    
     print("[*] Detailed scan results:")
-    print(detailed_scan_output)
+    while True:
+        output = process.stdout.readline()
+        if output:
+            line = output.strip()
+            print(f"    {line}")
+        if process.poll() is not None:
+            break
+
+
+def main(target, pn_flag, verbose):
+    quick_ports = quick_scan(target, pn_flag, verbose)
+    
+    if quick_ports:
+        detailed_scan(target, quick_ports, pn_flag, verbose)
+    
+    full_tcp_ports = full_tcp_scan(target, pn_flag, verbose)
+    full_udp_ports = full_udp_scan(target, pn_flag, verbose)
+    
+    if full_tcp_ports:
+        detailed_scan(target, full_tcp_ports, pn_flag, verbose)
+    
+    if full_udp_ports:
+        print(f"[*] Full UDP scan found open ports: {full_udp_ports}")
+        # Add additional actions for UDP ports if needed
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Automated Nmap Scanning')
     parser.add_argument('-t', '--target', required=True, help='Target IP address')
+    parser.add_argument('-Pn', '--no-ping', action='store_true', help='No ping, treats all hosts as online')
+    parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose output')
     args = parser.parse_args()
 
-    main(args.target)
+    main(args.target, args.no_ping, args.verbose)
